1ce6f29f1124baeaacfc37f282af0063a3eeb242
Santiago <david.chisco@mail.escuelaing.edu.co>
2016-03-04 12:33:36 -0500

    Correcion en la  documentacion la expecion

--- src/main/java/edu/eci/pdsw/samples/services/ServiciosPacientes.java
+++ src/main/java/edu/eci/pdsw/samples/services/ServiciosPacientes.java
@@ -42,7 +42,7 @@ public abstract class ServiciosPacientes {
      * @param idPaciente identificador del paciente
      * @param tipoid tipo de identificación del paciente
      * @return el paciente con el identificador dado
-     * @throws ExcepcionServiciosPacientes  si el paciente no existe
+     * @throws ExcepcionServiciosPacientes si  el paciente no esta registrado.
      */
     public abstract Paciente consultarPaciente(int idPaciente,String tipoid) throws ExcepcionServiciosPacientes;
     
@@ -50,8 +50,7 @@ public abstract class ServiciosPacientes {
     /**
      * Registra un nuevo PASIENTE en el sistema
      * @param p El nuevo paciente
-     * @throws Exception si se presenta algún error lógico
-     * o de persistencia (por ejemplo, si el paciente ya existe).
+     * @throws ExcepcionServiciosPacientes si se presenta algún error de persistencia o si el paciente ya existe.
      */
     public abstract void registrarNuevoPaciente(Paciente p) throws ExcepcionServiciosPacientes;
     
